e5c65e4f2ff2e4a46426f829ccfb4756f4e5d12f
PashaShuShu <honcharenkopavel@gmai.com>
2022-05-14 20:43:00 +0300

    Added function _finMatrixOfGapValues.

---
+++ src/services/geneticServices/index.js
@@ -0,0 +1,38 @@
+import config from "../../config/index";
+
+class GeneticServices {
+  _getRandom = () =>
+    Math.round(Math.random() * 10) % 2 === 0
+      ? Math.random() * -1
+      : Math.random();
+
+  _finMatrixOfGapValues = (gep, step) => {
+    const { a, b } = gep;
+    const gapValue = b - a;
+    const stepsNumber = Math.round(gapValue / step);
+    const matrixOfValues = [];
+    for (let i = 0; i < stepsNumber; i++) {
+      const a_i = a + i * step;
+      for (let j = 0; j < stepsNumber; j++) {
+        const b_j = b + j * step;
+        matrixOfValues.push([a_i, b_j]);
+      }
+    }
+    return matrixOfValues;
+  };
+
+  findParameters = (stepsNumber, calculatePolynomial) => {
+    const { step, gep } = config;
+
+    const z = [];
+    for (let i = 0; i < stepsNumber; i++) {
+      z.push(this._getRandom());
+    }
+
+    return z;
+  };
+}
+
+const geneticServices = new GeneticServices();
+
+export default geneticServices;
